08eae55ca2c8eadd90fd8ba3fb97fedd99ed2c27
jcuribev <jcamilouribev@gmail.com>
2022-08-26 09:49:09 -0500

    cambios

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Home from './Components/Home';
+import { NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Home />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name='Articles' component={Articles}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ Components/Articles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Articles = () => {
+  return (
+    <SafeAreaView>
+        <Text>Artículos</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Articles
